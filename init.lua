da7375801c43ced9e02338d0f84cc257448d5371
YosaZiege <YosaZiege@gmail.com>
2025-04-09 20:35:00 +0100

    Nerd Fond Implementation , Modulisation of Neovim plugins, Lualine and File System plugins

--- init.lua
+++ init.lua
@@ -1,8 +1,4 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=3")
-vim.g.mapleader = " "
+
 
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -17,31 +13,16 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-
-local plugins = {
-   { "catppuccin/nvim" , name = "catppuccin" , priority = 1000 },
-   {
-      'nvim-telescope/telescope.nvim', tag = '0.1.5',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-   },
-   {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-}
-local opts = {}
-
-require("lazy").setup(plugins, opts) 
+require("vim-options")
+require("lazy").setup("plugins") 
 
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 
-local config = require("nvim-treesitter.configs")
-config.setup({
-   ensure_installed = {"lua",  "javascript", "go" },
-   highlight = { enable = true },
-   indent = { enable = true },
-
-})
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
 
 
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
+
+
+
